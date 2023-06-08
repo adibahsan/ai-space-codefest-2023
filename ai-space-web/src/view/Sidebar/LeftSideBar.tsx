b12f32cfb517e172e07d77b1a4dc75225123c77b
Adib Ahsan Chowdhury <adibc@hsenidmobile.com>
2023-06-09 01:25:25 +0600

    feat(ai-space-web): Task Counter Added

--- ai-space-web/src/view/Sidebar/LeftSideBar.tsx
+++ ai-space-web/src/view/Sidebar/LeftSideBar.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ActionView from "../ActionView.tsx";
+import useActions from "../../reducer/hooks/useActions.ts";
 
 const LeftSideBar = props => {
     const sidebarClass = props.isOpen ? "sidebar open" : "sidebar";
+    const [actionList, ,] = useActions()
+
+
     return (
         <div className={sidebarClass}>
             <ActionView/>
             <button onClick={props.toggleSidebar} className="sidebar-toggle">
                 Actions
+                {actionList?.actions?.length > 0 && <span style={{left: "12px"}} className="notification-counter">{actionList?.actions?.length ?? 0}</span>}
             </button>
         </div>
     );
--- ai-space-web/src/view/Sidebar/RightSideBar.tsx
+++ ai-space-web/src/view/Sidebar/RightSideBar.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import TrackerView from "../TrackerView.tsx";
+import useTasks from "../../reducer/hooks/useTasks.ts";
 
 const RightSideBar = props => {
     const sidebarClass = props.isOpen ? "sidebar-right open" : "sidebar-right";
+    const [taskList, , updateTaskList] = useTasks()
+
+
+    let filteredTasks = taskList?.tasks?.filter(it => !it.status)
+
+    console.log("TaskList", taskList, filteredTasks)
+
     return (
         <div className={sidebarClass}>
             <TrackerView/>
             <button onClick={props.toggleSidebar} className="sidebar-toggle-right">
                 Tasks
+                {filteredTasks?.length > 0 && <span className="task-notification-counter">{filteredTasks?.length ?? 0}</span>}
             </button>
         </div>
     );
